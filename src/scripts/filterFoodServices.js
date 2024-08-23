f47f51ac6082dd412812774138730668d4122ca8
TorSdayur <tor.sdayur@gmail.com>
2024-08-23 00:33:49 -0400

    implemented availabilities filtering

--- src/scripts/filterFoodServices.js
+++ src/scripts/filterFoodServices.js
@@ -2,23 +2,35 @@ export default function filterFoodServices(
     foodServices,
     boroughs,
     dist,
-    availability,
+    availabilities,
     setFoodServices)
 {
     let filteredFoodServices = foodServices;
     if (boroughs)
     {
-        filteredFoodServices = filterFoodServices.filter(
+        filteredFoodServices = filteredFoodServices.filter(
             (foodService) => boroughs.includes(foodService.borough));
     }
     if (dist)
     {
         
     }
-    if (availability)
+    if (availabilities)
     {
-        
+        filteredFoodServices = filteredFoodServices.filter(
+            (foodService) => filterAvailabilities(availabilities, foodService));
     }
 
     setFoodServices(filteredFoodServices);
+}
+
+function filterAvailabilities(availabilities, foodService) {
+    for (const availability of foodService.availabilities)
+    {
+        if (availabilities.includes(availability))
+        {
+            return true;
+        }
+    }
+    return false;
 }
