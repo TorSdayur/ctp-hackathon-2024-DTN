72f937fa033c10e63224ab0465e4fd14bfae069c
Douglas Rollman <Douglaswrollman@gmail.com>
2025-05-31 10:29:46 -0400

    reactivated supabase under new project as old was not restorable

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 //const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 //const supabase = createClient(supabaseUrl, supabaseKey);
 const supabase = createClient(
-    "https://gadbhrsupsghumaigngg.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhZGJocnN1cHNnaHVtYWlnbmdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQyODIzNjEsImV4cCI6MjAzOTg1ODM2MX0.6jMNgFs_BV-dddLV3ifi5EburlqoA4SpT84ITfwkmMQ"
+    "https://bhbgpqnqzcvkuiowmpwc.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoYmdwcW5xemN2a3Vpb3dtcHdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2OTYwMDMsImV4cCI6MjA2NDI3MjAwM30.ZnRSLaIlQ8lHYEEaZhZYoYAxFfgSONiqH-1vRWHIFt8"
 );
 export default supabase;
